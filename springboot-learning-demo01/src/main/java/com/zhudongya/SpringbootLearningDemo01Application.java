1872976cd274748299db3eebfcc95ee5d14c4cb6
ZhuDongya1993 <1142086172@qq.com>
2018-06-13 10:30:44 +0800

    add demo01

---
+++ springboot-learning-demo01/src/main/java/com/zhudongya/SpringbootLearningDemo01Application.java
@@ -0,0 +1,12 @@
+package com.zhudongya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningDemo01Application.class, args);
+	}
+}
